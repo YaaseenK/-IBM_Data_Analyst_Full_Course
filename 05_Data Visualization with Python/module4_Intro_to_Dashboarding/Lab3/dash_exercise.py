ea59a964dc5b83a843a2359ea74fe03c6f4c16b8
Yaaseen <yaaseenkhan02@gmail.com>
2025-04-10 21:58:09 -0400

    added callback function

--- 05_Data Visualization with Python/module4_Intro_to_Dashboarding/Lab3/dash_exercise.py
+++ 05_Data Visualization with Python/module4_Intro_to_Dashboarding/Lab3/dash_exercise.py
@@ -1,23 +1,61 @@
 # Import required libraries
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html
 from dash.dependencies import Input, Output
-# Read the airline data into pandas dataframe
-airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
-                    encoding = "ISO-8859-1",
-                    dtype={'Div1Airport': str, 'Div1TailNum': str, 
-                            'Div2Airport': str, 'Div2TailNum': str})
 
+# Read the airline data into pandas dataframe
+airline_data = pd.read_csv(
+    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+    encoding="ISO-8859-1",
+    dtype={
+        'Div1Airport': str, 'Div1TailNum': str,
+        'Div2Airport': str, 'Div2TailNum': str
+    }
+)
+
+# Create a Dash app instance
 app = dash.Dash(__name__)
-app.layout = html.Div(children=[ html.H1('Total number of flights to the destination state split by reporting airline',
-                            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
-                            html.Div(["Input Year: ", dcc. Input(id='input-year',value='2010',
-                            type='number', style={'height':'50px', 'font-size': 35}),], 
-                            style={'font-size': 40}),html.Br(), html.Br(),
-                            html.Div(dcc.Graph(id='bar-plot')),]) 
-@app.callback( Output(component_id='bar-plot',component_property='figure'),
-               Input(component_id='input-year', component_property='value'))
+
+# App layout
+app.layout = html.Div(children=[
+    html.H1('Total number of flights to the destination state split by reporting airline',
+            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+    
+    html.Div([
+        "Input Year: ",
+        dcc.Input(id='input-year', value='2010', type='number',
+                  style={'height': '50px', 'font-size': 35}),
+    ], style={'font-size': 40}),
+
+    html.Br(),
+    html.Br(),
+
+    html.Div(dcc.Graph(id='bar-plot'))
+])
+
+# Define the callback
+@app.callback(
+    Output(component_id='bar-plot', component_property='figure'),
+    Input(component_id='input-year', component_property='value')
+)
+def get_graph(entered_year):
+    # Filter the data
+    df = airline_data[airline_data['Year'] == int(entered_year)]
+
+    # Group the data
+    bar_data = df.groupby(['Reporting_Airline'])['Flights'].sum().reset_index()
+
+    # Create a bar plot
+    fig = px.bar(bar_data,
+                    x='Reporting_Airline',
+                    y='Flights',
+                    title=f'Total flights in {entered_year} by Airline',
+                    labels={'Flights': 'Total Flights'},
+                    color='Reporting_Airline')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
